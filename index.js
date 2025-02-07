6659cc3e255560d155a9e2bd900fb6d27ff41a51
scarvage <mojha001@gmail.com>
2025-02-07 11:58:05 +0530

    db push

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const MONGO_URI = process.env.MONGO_URI;
+
 dotenv.config(); 
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
